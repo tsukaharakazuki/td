711c07e1cf46b760633a91f9c24fc388388c96e9
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2019-10-27 23:41:46 +0900

    Update pvuu_click_total.sql

--- tieup_report_gtm/query/pvuu_click_total.sql
+++ tieup_report_gtm/query/pvuu_click_total.sql
@@ -1,6 +1,10 @@
 SELECT
    pv AS total_pv
   ,uu AS total_uu
+  ,pv_pc AS total_pv_pc
+  ,uu_pc AS total_uu_pc
+  ,pv_sp AS total_pv_sp
+  ,uu_sp AS total_pv_sp
   ,c_1_cnt
   ,c_1
   ,c_2_cnt
@@ -19,6 +23,10 @@ FROM
   SELECT
      a.pv
     ,a.uu
+    ,a.pv_pc
+    ,a.uu_pc
+    ,a.pv_sp
+    ,a.uu_sp
     ,b.c_1_cnt
     ,b.c_1
     ,b.c_2_cnt
@@ -31,7 +39,8 @@ FROM
     ,b.c_5
     ,b.click_total_cnt
   FROM
-    pvuu_total_tmp_${td.each.db_client_name}_${td.each.db_label} AS a
-  LEFT JOIN click_tmp2_${td.each.db_client_name}_${td.each.db_label} AS b
+    pvuu_total_tmp2_${td.each.db_client_name}_${td.each.db_label} AS a
+  LEFT JOIN 
+    click_tmp2_${td.each.db_client_name}_${td.each.db_label} AS b
   ON a.id = b.id
   )
