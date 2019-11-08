38e4547bcea06e251c3124943e388ecd398740c7
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2019-11-08 12:09:20 +0900

    Update click_2.sql

--- tieup_report_gtm/query/click_2.sql
+++ tieup_report_gtm/query/click_2.sql
@@ -12,7 +12,7 @@ FROM
   WHERE
     regexp_like(td_path,'${td.each.article_id}') AND
     ${click_col} <> '' AND
-    regexp_like(${click_col},'${td.each.c_1}') AND
+    regexp_like(${click_col},'${td.each.c_2}') AND
     TD_TIME_RANGE(time,
       '${td.each.start_date}',,
       '${td.each.end_date}',
