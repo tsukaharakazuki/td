d850ab988231be146cfa8de3d59b5147206e5dd8
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2019-11-12 14:58:53 +0900

    Update member_duration_range.sql

--- tieup_report_user_ processing/query/member_duration_range.sql
+++ tieup_report_user_ processing/query/member_duration_range.sql
@@ -2,7 +2,7 @@ SELECT
   duration_range AS member_duration_range,
   sex AS member_duration_range_sex ,
   COUNT(1) AS member_duration_range_cnt ,
-  'member_duration_range' AS label
+  'attribute' AS label
 FROM
   user_${td.each.db_client_name}_${td.each.db_label}
 GROUP BY
