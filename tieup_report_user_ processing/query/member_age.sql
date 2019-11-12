fd3b13ad9338bec68047fbbec39ef73a470ac6b2
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2019-11-12 14:58:20 +0900

    Update member_age.sql

--- tieup_report_user_ processing/query/member_age.sql
+++ tieup_report_user_ processing/query/member_age.sql
@@ -2,7 +2,7 @@ SELECT
   age AS member_age,
   sex AS member_age_sex ,
   COUNT(1) AS member_age_cnt ,
-  'member_age' AS label
+  'attribute' AS label
 FROM
   user_${td.each.db_client_name}_${td.each.db_label}
 GROUP BY
