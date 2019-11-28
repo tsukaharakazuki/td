aa5e6348c821bcc9612e90521743572ea6007a1e
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2019-11-28 18:06:15 +0900

    Update click_3.sql

--- tieup_report_gtm/query/click_3.sql
+++ tieup_report_gtm/query/click_3.sql
@@ -14,7 +14,7 @@ FROM
     ${click_col} <> '' AND
     regexp_like(${click_col},'${td.each.c_}') AND
     TD_TIME_RANGE(time,
-      '${td.each.start_date}',,
+      '${td.each.start_date}',
       '${td.each.end_date}',
       'JST')
   )
