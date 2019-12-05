95818de159ec02c397a7d4f5cb9c35406afde7c2
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2019-12-05 14:13:22 +0900

    Update browser.sql

--- tieup_report_gtm/query/browser.sql
+++ tieup_report_gtm/query/browser.sql
@@ -1,8 +1,9 @@
 SELECT
+  TD_TIME_FORMAT(time, 'yyyy-MM-dd', 'JST') AS select_time ,
   uaip_browser ,
   label ,
   SUM(uaip_cnt) AS uaip_browser_cnt
 FROM 
   uaip_${td.each.db_client_name}_${td.each.db_label}
 GROUP BY
-  1,2
+  1,2,3
