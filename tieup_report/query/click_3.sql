a466dfd2110774ae80291fa63d488f64cdbd8b41
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2020-02-06 18:27:37 +0900

    Update click_3.sql

--- tieup_report/query/click_3.sql
+++ tieup_report/query/click_3.sql
@@ -12,7 +12,7 @@ FROM
   WHERE
     regexp_like(td_path,'${td.each.article_id}') AND
     ${click_col} <> '' AND
-    regexp_like(${click_col},'${td.each.c_}') AND
+    regexp_like(${click_col},'${td.each.c_3}') AND
     TD_TIME_RANGE(time,
       '${td.each.start_date}',
       '${td.each.end_date}',
