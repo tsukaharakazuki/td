e7b556d9fd36cb0ea228585e15e3d6771f6a207c
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2020-09-16 21:33:25 +0900

    Add files via upload

---
+++ sql_storage/stack_count.sql
@@ -0,0 +1,30 @@
+
+WITH
+
+
+base AS
+(
+  SELECT
+    TD_TIME_FORMAT(time, 'yyyy-MM-dd', 'JST') AS purchase_day ,
+    COUNT(*) AS cnt
+  FROM
+    TARGET_DATA_TABLE
+  GROUP BY
+    1
+)
+
+
+SELECT 
+  purchase_day
+  , cnt
+  , SUM(cnt) OVER (ORDER BY purchase_day ASC ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as user_sum
+FROM
+  (
+    SELECT
+      purchase_day
+      , sum(cnt) as cnt
+    FROM
+      base
+    group by
+      1
+  )
