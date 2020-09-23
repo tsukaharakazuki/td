4d24d1dff9fae8c34faf06dbcb52ad44a25306a4
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2020-09-23 17:31:51 +0900

    Update dst_match_count.sql

--- cookie_matching/query/dst_match_count.sql
+++ cookie_matching/query/dst_match_count.sql
@@ -1,7 +1,7 @@
 SELECT
   MAX(${name_flag}_all_cookie) AS ${name_flag}_all_cookie ,
   MAX(${name_flag}_user_cookie) AS ${name_flag}_user_cookie ,
-  MAX(${client.client_name}_cookie) AS ${client.client_name}_cookie ,
+  MAX(${client.client_name}_cookie) AS client_cookie ,
   MAX(match_cookie) AS match_cookie ,
   MAX(match_user_cookie) AS match_user_cookie ,
   (MAX(match_cookie) * 1.0) / (MAX(${client.client_name}_cookie) * 1.0) AS cookie_match_rate ,
