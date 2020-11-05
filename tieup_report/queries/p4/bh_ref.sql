f10e62b3ac4aa0765b80f9f283cc11e9b6729fd2
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2020-11-05 22:12:48 +0900

    Update bh_ref.sql

--- tieup_report/queries/p4/bh_ref.sql
+++ tieup_report/queries/p4/bh_ref.sql
@@ -8,6 +8,7 @@ FROM
   bh_${td.each.db_client_name}_${td.each.db_label}
 WHERE
   ${td.each.check_host_flag}td_host IN ('${td.each.check_host}') AND
+  regexp_like(td_path,'${td.each.article_id}') AND
   TD_TIME_RANGE(time,
     '${td.each.start_date}',
     '${td.each.end_date}',
