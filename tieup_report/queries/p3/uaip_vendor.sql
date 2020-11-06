cb517e7f26202abcacd2acaf28c72feabe22e5fc
tsukaharakazuki <tsukahara@treasure-data.com>
2020-11-06 23:03:52 +0900

    remove tieup_report_gtm

--- tieup_report/queries/p3/uaip_vendor.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  TD_TIME_FORMAT(time, 'yyyy-MM-dd', 'JST') AS select_time ,
-  uaip_vendor ,
-  label ,
-  SUM(uaip_cnt) AS uaip_vendor_cnt
-FROM 
-  uaip_${td.each.db_client_name}_${td.each.db_label}
-GROUP BY
-  1,2,3
