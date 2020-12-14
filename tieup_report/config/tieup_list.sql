0b4cbd4e64fb6ab5645b0f3130573502e0aed9b9
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2020-12-14 23:29:07 +0900

    Update tieup_list.sql

--- tieup_report/config/tieup_list.sql
+++ tieup_report/config/tieup_list.sql
@@ -1,7 +1,7 @@
 SELECT
   article_id ,
-  REGEXP_REPLACE(start_date, '(?<=^.{10}).*','') AS start_date ,
-  REGEXP_REPLACE(end_date, '(?<=^.{10}).*','') AS end_date ,
+  start_date ,
+  end_date ,
   check_host_flag ,
   check_host ,
   c_1 ,
@@ -9,87 +9,12 @@ SELECT
   c_3 ,
   c_4 ,
   c_5 ,
-  translate(
-    REPLACE(
-      REPLACE(
-        REPLACE(
-          REPLACE(
-            REPLACE(
-              REPLACE(
-                REPLACE(
-                  REPLACE(
-                    REPLACE(
-                      REPLACE(
-                        REPLACE(
-                          REPLACE(
-                            REPLACE(
-                              REPLACE(
-                                REPLACE(
-                                  REPLACE(
-                                    REPLACE(
-                                      REPLACE(db_client_name,'-','_') 
-                                    ,' ','')
-                                  ,'　','') 
-                                ,'&','and')
-                              ,'%','')
-                            ,'?','')
-                          ,'@','at')
-                        ,'(','')
-                      ,')','')
-                    ,'+','plus')
-                  ,'/','')
-                ,'<','')
-              ,'>','')
-            ,'*','')
-          ,'=','')
-        ,'¥','')
-      ,'!','')
-    ,'#',''),
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  'abcdefghijklmnopqrstuvwxyz'
-  ) AS db_client_name ,
-  translate(
-    REPLACE(
-      REPLACE(
-        REPLACE(
-          REPLACE(
-            REPLACE(
-              REPLACE(
-                REPLACE(
-                  REPLACE(
-                    REPLACE(
-                      REPLACE(
-                        REPLACE(
-                          REPLACE(
-                            REPLACE(
-                              REPLACE(
-                                REPLACE(
-                                  REPLACE(
-                                    REPLACE(
-                                      REPLACE(db_label,'-','_') 
-                                    ,' ','')
-                                  ,'　','') 
-                                ,'&','and')
-                              ,'%','')
-                            ,'?','')
-                          ,'@','at')
-                        ,'(','')
-                      ,')','')
-                    ,'+','plus')
-                  ,'/','')
-                ,'<','')
-              ,'>','')
-            ,'*','')
-          ,'=','')
-        ,'¥','')
-      ,'!','')
-    ,'#',''),
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  'abcdefghijklmnopqrstuvwxyz'
-  ) AS db_label ,
+  db_client_name ,
+  db_label ,
   ls_page_url ,
   ls_client_name_jp ,
   ls_page_title ,
   media_name
 FROM 
-  ${ls_db}.${ls_tb}
+  install_tieup_list
+
