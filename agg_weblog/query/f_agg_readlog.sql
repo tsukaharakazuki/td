4a8b58f2201cd90fdacb6fb68ff61d4d7704b227
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2021-05-26 11:50:14 +0900

    Update f_agg_readlog.sql

--- agg_weblog/query/f_agg_readlog.sql
+++ agg_weblog/query/f_agg_readlog.sql
@@ -5,8 +5,8 @@ WITH t1 AS　(
     cookie ,
     td_client_id ,
     td_global_id ,
-    ${media.check_td_ssc_id}  td_ssc_id ,
-    ${media.check_user_id}  ${media.user_id} AS user_id ,
+    ${media.td_ssc_id} AS td_ssc_id ,
+    ${media.user_id} AS user_id ,
     parse_url(td_url,'QUERY','utm_campaign') as utm_campaign ,
     parse_url(td_url,'QUERY','utm_medium') as utm_medium ,
     parse_url(td_url,'QUERY','utm_source') as utm_source ,
@@ -33,6 +33,7 @@ WITH t1 AS　(
     TD_IP_TO_LEAST_SPECIFIC_SUBDIVISION_NAME(td_ip) AS prefectures ,
     TD_IP_TO_CITY_NAME(td_ip) AS city ,
     ${media.read_col} AS read
+    ${media.custom_param}
   FROM
     (
       SELECT
@@ -66,8 +67,8 @@ SELECT
   cookie ,
   td_client_id ,
   td_global_id ,
-  ${media.check_td_ssc_id}  td_ssc_id ,
-  ${media.check_user_id}  user_id ,
+  td_ssc_id ,
+  user_id ,
   utm_campaign ,
   utm_medium ,
   utm_source ,
@@ -368,5 +369,6 @@ SELECT
   END map_prefectures ,
   city ,
   read
+  ${media.custom_param}
 FROM
   t1
