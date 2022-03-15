4330473b8016dc9e3cf43cea5f039490383d95a0
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2022-03-15 21:04:08 +0900

    Create p31_attribute_engagement_score.sql

---
+++ agg_master_segment/query/p31_attribute_engagement_score.sql
@@ -0,0 +1,23 @@
+WITH base AS (
+  SELECT
+    td_ms_id ,
+    CAST(TRUNCATE((MAX(time)- TO_UNIXTIME(now()))/ 86400) AS INT) AS recency ,
+    COUNT(DISTINCT TD_TIME_FORMAT(time,'yyyy-MM-dd','JST')) AS frequency ,
+    COUNT(DISTINCT engagement_vols) AS volume
+    ${(Object.prototype.toString.call(media[params].add_calculation.columns) === '[object Array]')?','+media[params].add_calculation.columns.join():''}
+  FROM
+    ${media[params].output_db}.l2_master_segment_behavior
+  GROUP BY
+    1
+)
+
+SELECT
+  * ,
+  ntile(10) OVER (ORDER BY engagement_score DESC) AS decile
+FROM (
+  SELECT
+    * ,
+    log10(frequency * SQRT(volume)* (${td.last_results.progress} + recency + 1)) AS engagement_score
+  FROM
+    base
+)
