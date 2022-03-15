a9615dd8f239bcbf02bdafdfeb5826a1191d53d3
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2022-03-15 13:41:14 +0900

    Update p23_master_table_app.sql

--- agg_master_segment/query/p23_master_table_app.sql
+++ agg_master_segment/query/p23_master_table_app.sql
@@ -3,9 +3,9 @@ SELECT
   td_ms_id ,
   MAX_BY(ifa, time) AS ifa ,
   MAX_BY(ifa_type, time) AS ifa_type ,
-  ARRAY_AGG(ifa) AS ifas 
+  ARRAY_AGG(DISTINCT ifa) FILTER(WHERE ifa IS NOT NULL) AS ifas 
   --, MAX_BY(user_id, time) AS user_id ,
-  --ARRAY_AGG(user_id) AS user_ids 
+  --ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
 FROM 
   ${media[params].output_db}.l2_master_segment_behavior
 GROUP BY 
