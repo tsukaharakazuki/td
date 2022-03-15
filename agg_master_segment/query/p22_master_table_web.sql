6380ca059d9592c01e788d9c549fffe45b16eed6
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2022-03-15 13:40:09 +0900

    Update p22_master_table_web.sql

--- agg_master_segment/query/p22_master_table_web.sql
+++ agg_master_segment/query/p22_master_table_web.sql
@@ -3,7 +3,7 @@ SELECT
   td_ms_id ,
   MAX_BY(td_client_id, time) AS td_client_id ,
   MAX_BY(user_id, time) AS user_id ,
-  ARRAY_AGG(user_id) AS user_ids 
+  ARRAY_AGG(DISTINCT user_id) FILTER(WHERE user_id IS NOT NULL) AS user_ids 
 FROM 
   ${media[params].output_db}.l2_master_segment_behavior
 GROUP BY 
