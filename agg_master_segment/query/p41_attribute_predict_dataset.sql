5e2d87693bcfee45fe668d5cc2b6f1e337d6e79b
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2022-03-18 11:35:55 +0900

    Update p41_attribute_predict_dataset.sql

--- agg_master_segment/query/p41_attribute_predict_dataset.sql
+++ agg_master_segment/query/p41_attribute_predict_dataset.sql
@@ -3,12 +3,12 @@ WITH t0 AS (
     td_ms_id ,
     ${pred.colmuns} AS val ,
     CASE
-      WHEN COUNT(*) = 1 THEN 'views_1'
-      WHEN COUNT(*) BETWEEN 2 AND 5 THEN 'views_5'
+      WHEN COUNT(*) = 1 THEN 'views_01'
+      WHEN COUNT(*) BETWEEN 2 AND 5 THEN 'views_05'
       WHEN COUNT(*) BETWEEN 6 AND 15 THEN 'views_15'
       WHEN COUNT(*) BETWEEN 16 AND 30 THEN 'views_30'
       WHEN COUNT(*) BETWEEN 31 AND 50 THEN 'views_50'
-      WHEN COUNT(*) >= 51 THEN 'views_over50'
+      WHEN COUNT(*) >= 51 THEN 'views_51_over'
     END key 
   FROM
     ${media[params].output_db}.l2_master_segment_behavior
@@ -33,12 +33,12 @@ WITH t0 AS (
 
 SELECT 
   td_ms_id,
-  MAX(CASE WHEN key = 'views_1' THEN vals END) AS views_1_${pred.colmuns} ,
-  MAX(CASE WHEN key = 'views_5' THEN vals END) AS views_5_${pred.colmuns} ,
+  MAX(CASE WHEN key = 'views_01' THEN vals END) AS views_01_${pred.colmuns} ,
+  MAX(CASE WHEN key = 'views_05' THEN vals END) AS views_05_${pred.colmuns} ,
   MAX(CASE WHEN key = 'views_15' THEN vals END) AS views_15_${pred.colmuns} ,
   MAX(CASE WHEN key = 'views_30' THEN vals END) AS views_30_${pred.colmuns} ,
   MAX(CASE WHEN key = 'views_50' THEN vals END) AS views_50_${pred.colmuns} ,
-  MAX(CASE WHEN key = 'views_over50' THEN vals END) AS views_over50_${pred.colmuns}
+  MAX(CASE WHEN key = 'views_51_over' THEN vals END) AS views_51_over_${pred.colmuns}
 FROM 
   t1
 GROUP BY
