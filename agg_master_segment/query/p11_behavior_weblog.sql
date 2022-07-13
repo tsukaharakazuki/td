914da6cdd9de37020974b058b987ab3311f4efaf
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2022-07-13 16:07:17 +0900

    Update p11_behavior_weblog.sql

--- agg_master_segment/query/p11_behavior_weblog.sql
+++ agg_master_segment/query/p11_behavior_weblog.sql
@@ -2,7 +2,8 @@ SELECT
   'web' AS behavior_type ,
   ${media[params].key_id_web} AS td_ms_id ,
   ${media[params].set_td_url_web} AS td_url ,
-  td_url AS td_url_raw
+  td_url AS td_url_raw ,
+  ${media[params].engagement_vols_web} AS engagement_vols
   ${td.last_results.set_columns}
 FROM
   ${media[params].weblog_db}.${media[params].weblog_tbl}
