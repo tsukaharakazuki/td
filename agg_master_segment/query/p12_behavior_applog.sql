952de4a499fe3c5710b6300465a14717b93bf71e
tsukaharakazuki <tsukahara.kazuki@gmail.com>
2022-07-13 16:07:40 +0900

    Update p12_behavior_applog.sql

--- agg_master_segment/query/p12_behavior_applog.sql
+++ agg_master_segment/query/p12_behavior_applog.sql
@@ -2,6 +2,7 @@ SELECT
   'app' AS behavior_type ,
   ${media[params].key_id_app} AS td_ms_id ,
   ${media[params].set_td_url_app} AS td_url ,
+  ${media[params].engagement_vols_app} AS engagement_vols ,
   *
 FROM
   ${media[params].applog_db}.${media[params].applog_tbl}
